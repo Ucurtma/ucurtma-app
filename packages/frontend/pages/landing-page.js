e981dc70616188e43c980ae97ca8cb49ef6784c3
Mustafa Turhan <odunluzikkim@gmail.com>
2019-06-21 13:38:02 +0300

    add logo

---
+++ packages/frontend/pages/landing-page.js
@@ -0,0 +1,12 @@
+import Logo from '../icons/logo';
+
+function LandingPage() {
+  return (
+    <p>
+      <Logo />
+      hello, there will be a landing page.
+    </p>
+  );
+}
+
+export default LandingPage;

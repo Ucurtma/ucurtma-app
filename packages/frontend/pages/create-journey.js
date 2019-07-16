4b4738bcec7126c2e05a7aee3ebd071d4dbb5c21
Mustafa Turhan <odunluzikkim@gmail.com>
2019-07-16 13:20:24 +0300

    fix dependency cycle

--- packages/frontend/pages/create-journey.js
+++ packages/frontend/pages/create-journey.js
@@ -1,35 +1,6 @@
-import React, { createContext } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 import CreateJourneyContent from '../components/student-journey/create-journey-content';
-
-/*
-  TODO / DISCUSS: i had create reducers folder and make all files related with context before this one.
-  but, after little thought about it; i decided that it doesn't make sense.
-  nextjs project structure doesn't looks like CRA apps. we have pages folder in next.js.
-  so, why we shouldn't use it? actually, i don't know is it a problem or not.
-  we can discuss it later.
-*/
-
-const initialState = { activeStep: 0 };
-
-const reducer = (state, action) => {
-  switch (action.type) {
-    case 'setActiveStep':
-      return { ...state, activeStep: action.step };
-    default:
-      throw Error('I guess there is no type for create journey provider');
-  }
-};
-
-function CreateJourneyProvider({ children }) {
-  const [state, dispatch] = React.useReducer(reducer, initialState);
-
-  return (
-    <CreateJourneyCtx.Provider value={[state, dispatch]}>
-      {children}
-    </CreateJourneyCtx.Provider>
-  );
-}
+import CreateJourneyProvider from '../context/create-journey-context';
 
 function CreateJourney() {
   return (
@@ -39,9 +10,4 @@ function CreateJourney() {
   );
 }
 
-export const CreateJourneyCtx = createContext([initialState]);
 export default CreateJourney;
-
-CreateJourneyProvider.propTypes = {
-  children: PropTypes.node.isRequired,
-};

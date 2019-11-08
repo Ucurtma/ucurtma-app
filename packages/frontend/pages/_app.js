d81cb58a499f12912eeb7d6835a7e08b15cfd055
Mustafa Turhan <odunluzikkim@gmail.com>
2019-11-08 10:18:34 +0300

    add graphql

--- packages/frontend/pages/_app.js
+++ packages/frontend/pages/_app.js
@@ -1,40 +1,36 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
 import { Global } from '@emotion/core';
-import App from 'next/app';
 import Router from 'next/router';
 import trackPageView from '../utils/ga-tag';
 import customTheme from '../theme';
+import { withApollo } from '../utils/apollo';
 
-/* we're using class because react hooks isn't working in default configure pages yet */
-
-class MyApp extends App {
-  componentDidMount() {
+// eslint-disable-next-line react/prop-types
+function MyApp({ Component, pageProps }) {
+  useEffect(() => {
     Router.onRouteChangeComplete = url => {
       trackPageView(url);
     };
-  }
+  }, []);
 
-  render() {
-    const { Component, pageProps } = this.props;
-    return (
-      <ThemeProvider theme={customTheme}>
-        <CSSReset />
-        <Component {...pageProps} />
-        <Global
-          styles={{
-            html: {
-              fontFamily:
-                'Quicksand, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-            },
-            body: {
-              backgroundColor: '#F3F3F3',
-            },
-          }}
-        />
-      </ThemeProvider>
-    );
-  }
+  return (
+    <ThemeProvider theme={customTheme}>
+      <CSSReset />
+      <Component {...pageProps} />
+      <Global
+        styles={{
+          html: {
+            fontFamily:
+              'Quicksand, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+          },
+          body: {
+            backgroundColor: '#F3F3F3',
+          },
+        }}
+      />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp;
+export default withApollo(MyApp);

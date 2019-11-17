62f65ceffddf775390d8c8cfeea1edd212f07945
Mert Susur <msusur@gocardless.com>
2019-11-17 18:16:28 +0000

    Rename module name and identifier type

--- .aws/environments/non-prod/ucurtma-database.tf
+++ .aws/environments/non-prod/ucurtma-database.tf
@@ -1,48 +1,9 @@
-module "dynamodb_table" {
+module "non-prod-app-profiles-database-table" {
   source            = "../../modules/dynamodb"
   namespace         = "uc"
   stage             = "non-prod"
   name              = "ucurtma-profiles-non-prod"
-  hash_key          = "UserId"
-  hash_key_type     = "N"
-  range_key         = "RangeKey"
+  hash_key          = "Email"
+  hash_key_type     = "S"
   enable_autoscaler = false
-
-  dynamodb_attributes = [
-    {
-      name = "Email"
-      type = "S"
-    },
-    {
-      name = "Timestamp",
-      type = "S"
-    }
-  ]
-
-  local_secondary_index_map = [
-    {
-      name               = "TimestampSortIndex"
-      range_key          = "Timestamp"
-      projection_type    = "INCLUDE"
-      non_key_attributes = ["UserId", "Email"]
-    },
-    {
-      name               = "EmailIndex"
-      range_key          = "Timestamp"
-      projection_type    = "INCLUDE"
-      non_key_attributes = ["UserId", "RangeKey"]
-    }
-  ]
-
-  global_secondary_index_map = [
-    {
-      name               = "DailyAverageIndex"
-      hash_key           = "Email"
-      range_key          = "RangeKey"
-      write_capacity     = 5
-      read_capacity      = 5
-      projection_type    = "INCLUDE"
-      non_key_attributes = ["UserId", "Email"]
-    }
-  ]
 }

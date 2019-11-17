7aedc3a7ff2b95d94a259a4f240554af427c8761
Mert Susur <msusur@gocardless.com>
2019-11-17 12:38:06 +0000

    Adds Dynamodb table resource for non-prod

---
+++ .aws/modules/dynamodb/variables_input.tf
@@ -0,0 +1,187 @@
+variable "aws_region" {
+  type        = "string"
+  default     = "eu-west-2"
+  description = "(Optional) AWS Region, e.g. us-east-1. Used as CodeBuild ENV variable when building Docker images. For more info: http://docs.aws.amazon.com/codebuild/latest/userguide/sample-docker.html"
+}
+
+variable "namespace" {
+  type        = string
+  description = "Namespace (e.g. `eg` or `cp`)"
+  default     = ""
+}
+
+variable "stage" {
+  type        = string
+  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
+  default     = ""
+}
+
+variable "enabled" {
+  type        = bool
+  description = "Set to false to prevent the module from creating any resources"
+  default     = true
+}
+
+variable "name" {
+  type        = string
+  description = "Name  (e.g. `app` or `cluster`)"
+}
+
+variable "delimiter" {
+  type        = string
+  default     = "-"
+  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
+}
+
+variable "attributes" {
+  type        = list(string)
+  default     = []
+  description = "Additional attributes (e.g. `policy` or `role`)"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
+}
+
+variable "autoscale_write_target" {
+  type        = number
+  default     = 50
+  description = "The target value (in %) for DynamoDB write autoscaling"
+}
+
+variable "autoscale_read_target" {
+  type        = number
+  default     = 50
+  description = "The target value (in %) for DynamoDB read autoscaling"
+}
+
+variable "autoscale_min_read_capacity" {
+  type        = number
+  default     = 5
+  description = "DynamoDB autoscaling min read capacity"
+}
+
+variable "autoscale_max_read_capacity" {
+  type        = number
+  default     = 20
+  description = "DynamoDB autoscaling max read capacity"
+}
+
+variable "autoscale_min_write_capacity" {
+  type        = number
+  default     = 5
+  description = "DynamoDB autoscaling min write capacity"
+}
+
+variable "autoscale_max_write_capacity" {
+  type        = number
+  default     = 20
+  description = "DynamoDB autoscaling max write capacity"
+}
+
+variable "billing_mode" {
+  type        = string
+  default     = "PROVISIONED"
+  description = "DynamoDB Billing mode. Can be PROVISIONED or PAY_PER_REQUEST"
+}
+
+variable "enable_streams" {
+  type        = bool
+  default     = false
+  description = "Enable DynamoDB streams"
+}
+
+variable "stream_view_type" {
+  type        = string
+  default     = ""
+  description = "When an item in the table is modified, what information is written to the stream"
+}
+
+variable "enable_encryption" {
+  type        = bool
+  default     = true
+  description = "Enable DynamoDB server-side encryption"
+}
+
+variable "enable_point_in_time_recovery" {
+  type        = bool
+  default     = true
+  description = "Enable DynamoDB point in time recovery"
+}
+
+variable "hash_key" {
+  type        = string
+  description = "DynamoDB table Hash Key"
+}
+
+variable "hash_key_type" {
+  type        = string
+  default     = "S"
+  description = "Hash Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
+}
+
+variable "range_key" {
+  type        = string
+  default     = ""
+  description = "DynamoDB table Range Key"
+}
+
+variable "range_key_type" {
+  type        = string
+  default     = "S"
+  description = "Range Key type, which must be a scalar type: `S`, `N`, or `B` for (S)tring, (N)umber or (B)inary data"
+}
+
+variable "ttl_attribute" {
+  type        = string
+  default     = "Expires"
+  description = "DynamoDB table TTL attribute"
+}
+
+variable "enable_autoscaler" {
+  type        = string
+  default     = "true"
+  description = "Flag to enable/disable DynamoDB autoscaling"
+}
+
+variable "dynamodb_attributes" {
+  type = list(object({
+    name = string
+    type = string
+  }))
+  default     = []
+  description = "Additional DynamoDB attributes in the form of a list of mapped values"
+}
+
+variable "global_secondary_index_map" {
+  type = list(object({
+    hash_key           = string
+    name               = string
+    non_key_attributes = list(string)
+    projection_type    = string
+    range_key          = string
+    read_capacity      = number
+    write_capacity     = number
+  }))
+  default     = []
+  description = "Additional global secondary indexes in the form of a list of mapped values"
+}
+
+variable "local_secondary_index_map" {
+  type = list(object({
+    name               = string
+    non_key_attributes = list(string)
+    projection_type    = string
+    range_key          = string
+  }))
+  default     = []
+  description = "Additional local secondary indexes in the form of a list of mapped values"
+}
+
+variable "regex_replace_chars" {
+  type        = string
+  default     = "/[^a-zA-Z0-9-]/"
+  description = "Regex to replace chars with empty string in `namespace`, `environment`, `stage` and `name`. By default only hyphens, letters and digits are allowed, all other chars are removed"
+}

e0a4d9c20dd7ca9991a0a8b57906c44eeeb85406
Mustafa Turhan <odunluzikkim@gmail.com>
2019-11-23 13:32:04 +0300

    change 'name' keyword with 'fullname'

--- packages/frontend/components/ui/settings/change-password.js
+++ packages/frontend/components/ui/settings/change-password.js
@@ -31,13 +31,13 @@ const passwordChangeSchema = Yup.object().shape({
 
 const CHANGE_PASSWORD = gql`
   mutation updatePassword(
-    $userId: Int!
+    $email: String!
     $currentPassword: String!
     $newPassword: String!
     $repeatNewPassword: String!
   ) {
     updatePassword(
-      userId: $userId
+      email: $email
       currentPassword: $currentPassword
       newPassword: $newPassword
       repeatNewPassword: $repeatNewPassword
@@ -68,7 +68,7 @@ function ChangePassword({ withTitle }) {
           setSubmitting(true);
           await changePassword({
             variables: {
-              userId: Math.floor(Math.random() * 100), // todo: get userID from db when it is ready
+              email: Math.floor(Math.random() * 100), // todo: get email from db when it is ready
               currentPassword: values.currentPassword,
               newPassword: values.newPassword,
               repeatNewPassword: values.repeatNewPassword,

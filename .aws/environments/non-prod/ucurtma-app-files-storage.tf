b2556a76cf42191f2fdb46edc5c840d3bcf1f5e1
Mert Susur <msusur@gocardless.com>
2019-11-23 18:37:56 +0000

    Adds the new restricted bucket module

--- .aws/environments/non-prod/ucurtma-app-files-storage.tf
+++ .aws/environments/non-prod/ucurtma-app-files-storage.tf
@@ -1,7 +1,6 @@
 module "non-prod-app-files-storage" {
-  source        = "../../modules/s3/web_hosting_s3_bucket"
-  bucket        = "non-prod.files.ucurtmaprojesi.com"
-  region        = "eu-west-2"
-  remote_state  = "${var.remote_state}"
-  web_page_user = "non-prod-app-files-storage-deployment-user"
+  source      = "../../modules/s3/bucket"
+  region      = "eu-west-2"
+  bucket      = "non-prod.files.ucurtmaprojesi.com"
+  bucket_user = "non-prod-app-files-storage-deployment-user"
 }
---
+++ .aws/modules/s3/bucket/variables_input.tf
@@ -0,0 +1,7 @@
+variable "bucket" {}
+
+variable "bucket_user" {}
+
+variable "region" {
+  default = "eu-west-2"
+}

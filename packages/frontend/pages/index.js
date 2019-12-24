4f9dfd82602bf1fd3ad177ffcb625782aaab1c67
Mustafa Turhan <odunluzikkim@gmail.com>
2019-12-24 23:45:57 +0300

    initial index page, without responsive features

--- packages/frontend/pages/index.js
+++ packages/frontend/pages/index.js
@@ -1,12 +1,67 @@
 import React from 'react';
-import Header from '../components/ui/header';
+import Link from 'next/link';
+import { Grid, Box, Icon, Flex, Heading, Text, Button } from '@chakra-ui/core';
+import { Navigation } from 'react-feather';
 
 function Home() {
   return (
-    <div>
-      <Header />
-      for now, there is a big silence. try one.
-    </div>
+    <Grid height="100vh" templateColumns="30% auto">
+      <Flex
+        mt="3rem"
+        ml="6rem"
+        mb="10rem"
+        flexDir="column"
+        justifyContent="space-between"
+        bg="red"
+      >
+        <Link href="/">
+          <a id="logo">
+            <Icon name="logo" size="4rem" />
+          </a>
+        </Link>
+        <Box pr="3rem">
+          <Heading maxW="253px" lineHeight="1">
+            İnsanlardan, insanlara.
+          </Heading>
+          <Text mt="1.5rem">
+            Daha iyi bir dünya yaratmak için birbirimize ihtiyacımız var.
+            Uçurtma, bizi birbirimize daha kolay ulaştırmak için var.
+          </Text>
+          <Button
+            float="right"
+            variant="ghost"
+            mt="1rem"
+            rightIcon="arrow-forward"
+          >
+            Nasıl çalışıyor?
+          </Button>
+        </Box>
+        <Flex>
+          <Button
+            float="right"
+            variant="solid"
+            mt="1rem"
+            bg="white"
+            h="66px"
+            w="115%"
+            flexShrink="0"
+            justifyContent="space-between"
+            boxShadow="0 0 12px rgba(124, 124, 124, 0.16)"
+          >
+            Kendin için bir kampanya yarat
+            <Icon as={Navigation} size="28px" mr="0.5rem" />
+          </Button>
+        </Flex>
+      </Flex>
+      <Box
+        w="100%"
+        bg="red"
+        backgroundImage='url("/background.svg")'
+        backgroundRepeat="no-repeat"
+        backgroundSize="cover"
+        backgroundPosition="center center"
+      />
+    </Grid>
   );
 }
 

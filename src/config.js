b667816a4e0b8f32e74f7434069ad004d42393ee
Mert Susur <msusur@gocardless.com>
2020-03-22 20:51:46 +0000

    Reverse configuration settings for prod

--- src/config.js
+++ src/config.js
@@ -1,10 +1,10 @@
-export const isDevelopment =
-  process.env.NODE_ENV === 'development' || process.argv.ENV === 'development';
+export const isProduction =
+  process.env.NODE_ENV === 'prod' || process.argv.ENV === 'prod';
 
 export const gaTrackingId = 'UA-143538110-1';
 
 const config = {
-  endpoint: isDevelopment
+  endpoint: !isProduction
     ? 'https://ucurtma-backend-test.herokuapp.com/graphql'
     : 'https://api.ucurtmaprojesi.com/graphql',
   recaptcha: '6LdMmM0UAAAAAGk0QRoBV06-jmLTPOuj9xo5OTnz',

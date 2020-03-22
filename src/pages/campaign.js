690036b34326edaf8c84971ed842b2a3fa745db4
Mustafa Turhan <odunluzikkim@gmail.com>
2020-03-22 23:20:41 +0300

    make bank transfer flow inline

--- src/pages/campaign.js
+++ src/pages/campaign.js
@@ -45,7 +45,7 @@ const GET_CAMPAIGN = gql`
 `;
 
 function Campaign() {
-  const [content, setContent] = React.useState('donate');
+  const [content, setContent] = React.useState('markdown');
   const { id } = useParams();
   const { loading, error, data } = useQuery(GET_CAMPAIGN, {
     variables: { campaignId: id },
@@ -78,150 +78,156 @@ function Campaign() {
   }
 
   return (
-    <Box backgroundColor="red">
-      <Header mt={8} withLogo hideMenu />
-      <Container display="block">
-        <Helmet>
-          <title>
-            {data
-              ? `${data.campaign?.campaignTitle} - ${data.campaign?.student.name} - Uçurtma Projesi`
-              : 'Uçurtma Projesi'}
-          </title>
-        </Helmet>
-        <Flex
-          my={{ base: 2, md: 10 }}
-          justifyContent="space-between"
-          alignItems="center"
-          width="full"
-          flexDir={{ base: 'column', md: 'row' }}
-        >
-          <Flex mx={{ base: 4, md: 0 }} alignItems="flex-end" flexShrink="0">
-            {loading ? (
-              <Skeleton width={72} height={72} circle />
-            ) : (
-              <Avatar
-                size="lg"
-                src={data.campaign?.student.profilePhoto}
-                name={data.campaign?.student.name}
-              />
-            )}
-            <Box ml={4}>
-              <Heading size="sm" color="gray.600">
-                {loading ? (
-                  <Skeleton width={200} />
-                ) : (
-                  data.campaign?.student.name
-                )}
-              </Heading>
-              <Text color="gray.500">
-                {loading ? (
-                  <Skeleton width={260} />
-                ) : (
-                  <>
-                    {data.campaign?.student.school} /{' '}
-                    {data.campaign?.student.department}
-                  </>
-                )}
-              </Text>
-            </Box>
-          </Flex>
+    <Flex flexDir="column" justify="space-between" height="full">
+      <Box>
+        <Header mt={8} withLogo hideMenu />
+        <Container display="block">
+          <Helmet>
+            <title>
+              {data
+                ? `${data.campaign?.campaignTitle} - ${data.campaign?.student.name} - Uçurtma Projesi`
+                : 'Uçurtma Projesi'}
+            </title>
+          </Helmet>
           <Flex
-            width={{ base: 'full', md: 'unset' }}
-            justify={{ base: 'space-around', md: 'inherit' }}
-            mt={{ base: 8, md: 0 }}
-            borderY={{ base: '1px solid', md: 0 }}
-            borderColor="gray.300"
-            borderTopColor="gray.300"
-            p={{ base: 4, md: 0 }}
-            bg={{ base: 'gray.100', md: 'inherit' }}
+            my={{ base: 2, md: 10 }}
+            justifyContent="space-between"
+            alignItems="center"
+            width="full"
+            flexDir={{ base: 'column', md: 'row' }}
           >
-            <Box pr={6} borderRight={{ md: '1px solid #CBD5E0' }}>
-              <Heading size="sm" color="gray.400">
-                {loading ? <Skeleton width={140} /> : 'Destekçi Sayısı'}
-              </Heading>
-              <Text
-                fontSize="1.5rem"
-                textAlign={{ base: 'center', md: 'left' }}
-                fontWeight={500}
-              >
-                {loading ? (
-                  <Skeleton width={70} />
-                ) : (
-                  data.campaign?.supporterCount
-                )}
-              </Text>
-            </Box>
-            <Box pl={6}>
-              <Heading size="sm" color="gray.400">
-                {loading ? <Skeleton width={140} /> : 'Toplam Destek'}
-              </Heading>
-              <Text
-                fontSize="1.5rem"
-                fontWeight={500}
-                textAlign={{ base: 'center', md: 'left' }}
-                color="linkBlue"
-              >
-                {loading ? (
-                  <Skeleton width={70} />
-                ) : (
-                  `₺${data.campaign?.totalFunds}`
-                )}
-              </Text>
-            </Box>
-          </Flex>
-        </Flex>
-        <Flex justify="space-between" align="center" mx={{ base: 4, md: 0 }}>
-          {loading ? (
-            <Box flex={1}>
-              <Skeleton height={72} />
-            </Box>
-          ) : (
-            <>
-              <Heading color="gray.700">{data.campaign?.campaignTitle}</Heading>
-              <Button
-                variant="solid"
-                bg="gray.100"
-                h={16}
-                width={{ base: 'auto', md: '368px' }}
-                flexShrink="0"
-                justifyContent="space-between"
-                boxShadow="0 0 12px rgba(124, 124, 124, 0.16)"
-                onClick={() => setContent('donate')}
-                zIndex={2000}
-              >
-                Destek Ol
-                <Icon as={Award} size="28px" />
-              </Button>
-            </>
-          )}
-        </Flex>
-        <Container
-          px={{ base: 4, md: 0 }}
-          pt={{ space: 4 }}
-          pb={{ space: 4 }}
-          display="block"
-        >
-          {loading ? (
-            <Skeleton count={12} />
-          ) : (
-            <>
-              {content === 'markdown' && (
-                <ReactMarkdown
-                  renderers={ChakraUIRenderer()}
-                  source={data.campaign?.campaignText}
-                  escapeHtml={false}
+            <Flex mx={{ base: 4, md: 0 }} alignItems="flex-end" flexShrink="0">
+              {loading ? (
+                <Skeleton width={72} height={72} circle />
+              ) : (
+                <Avatar
+                  size="lg"
+                  src={data.campaign?.student.profilePhoto}
+                  name={data.campaign?.student.name}
                 />
               )}
-
-              {content === 'donate' && (
-                <Donate ethereumAddress={data.campaign?.ethereumAddress} />
-              )}
-            </>
-          )}
+              <Box ml={4}>
+                <Heading size="sm" color="gray.600">
+                  {loading ? (
+                    <Skeleton width={200} />
+                  ) : (
+                    data.campaign?.student.name
+                  )}
+                </Heading>
+                <Text color="gray.500">
+                  {loading ? (
+                    <Skeleton width={260} />
+                  ) : (
+                    <>
+                      {data.campaign?.student.school} /{' '}
+                      {data.campaign?.student.department}
+                    </>
+                  )}
+                </Text>
+              </Box>
+            </Flex>
+            <Flex
+              width={{ base: 'full', md: 'unset' }}
+              justify={{ base: 'space-around', md: 'inherit' }}
+              mt={{ base: 8, md: 0 }}
+              borderY={{ base: '1px solid', md: 0 }}
+              borderColor="gray.300"
+              borderTopColor="gray.300"
+              p={{ base: 4, md: 0 }}
+              bg={{ base: 'gray.100', md: 'inherit' }}
+            >
+              <Box pr={6} borderRight={{ md: '1px solid #CBD5E0' }}>
+                <Heading size="sm" color="gray.400">
+                  {loading ? <Skeleton width={140} /> : 'Destekçi Sayısı'}
+                </Heading>
+                <Text
+                  fontSize="1.5rem"
+                  textAlign={{ base: 'center', md: 'left' }}
+                  fontWeight={500}
+                >
+                  {loading ? (
+                    <Skeleton width={70} />
+                  ) : (
+                    data.campaign?.supporterCount
+                  )}
+                </Text>
+              </Box>
+              <Box pl={6}>
+                <Heading size="sm" color="gray.400">
+                  {loading ? <Skeleton width={140} /> : 'Toplam Destek'}
+                </Heading>
+                <Text
+                  fontSize="1.5rem"
+                  fontWeight={500}
+                  textAlign={{ base: 'center', md: 'left' }}
+                  color="linkBlue"
+                >
+                  {loading ? (
+                    <Skeleton width={70} />
+                  ) : (
+                    `₺${data.campaign?.totalFunds}`
+                  )}
+                </Text>
+              </Box>
+            </Flex>
+          </Flex>
+          <Flex justify="space-between" align="center" mx={{ base: 4, md: 0 }}>
+            {loading ? (
+              <Box flex={1}>
+                <Skeleton height={72} />
+              </Box>
+            ) : (
+              <>
+                <Heading color="gray.700">
+                  {data.campaign?.campaignTitle}
+                </Heading>
+                <Button
+                  variant="solid"
+                  bg="gray.100"
+                  h={16}
+                  width={{ base: 'auto', md: '368px' }}
+                  flexShrink="0"
+                  justifyContent="space-between"
+                  boxShadow="0 0 12px rgba(124, 124, 124, 0.16)"
+                  onClick={() => setContent('donate')}
+                  zIndex={2000}
+                >
+                  Destek Ol
+                  <Icon as={Award} size="28px" />
+                </Button>
+              </>
+            )}
+          </Flex>
+          <Container
+            px={{ base: 4, md: 0 }}
+            pt={{ space: 4 }}
+            pb={{ space: 4 }}
+            display="block"
+          >
+            {loading ? (
+              <Skeleton count={12} />
+            ) : (
+              <>
+                <Box display={content === 'markdown' ? 'block' : 'none'}>
+                  <ReactMarkdown
+                    renderers={ChakraUIRenderer()}
+                    source={data.campaign?.campaignText}
+                    escapeHtml={false}
+                  />
+                </Box>
+                <Box display={content === 'donate' ? 'block' : 'none'}>
+                  <Donate
+                    ethereumAddress={data.campaign?.ethereumAddress}
+                    onBack={() => setContent('markdown')}
+                  />
+                </Box>
+              </>
+            )}
+          </Container>
         </Container>
-      </Container>
+      </Box>
       <LandingFooter />
-    </Box>
+    </Flex>
   );
 }
 

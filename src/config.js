123650bb7d702b338f3c5464223179814c5bdfef
Mustafa Turhan <odunluzikkim@gmail.com>
2020-06-07 17:34:41 +0300

    revert configjs

--- src/config.js
+++ src/config.js
@@ -1,7 +1,9 @@
 export const isProduction = window && window.app_env === 'production';
 
 export const gaTrackingId = 'UA-143538110-1';
-const backendUrl = 'https://api.ucurtmaprojesi.com';
+const backendUrl = !isProduction
+  ? 'https://ucurtma-backend-test.herokuapp.com'
+  : 'https://api.ucurtmaprojesi.com';
 
 const config = {
   recaptcha: '6LdMmM0UAAAAAGk0QRoBV06-jmLTPOuj9xo5OTnz',

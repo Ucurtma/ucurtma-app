b475244ee6cffab04a021d4f1d3a7cb71a2816dc
Mustafa Turhan <odunluzikkim@gmail.com>
2020-06-09 11:28:08 +0300

    update font-weight by default, divide components and add info about bilira

--- src/components/view/campaign/campaign-content.js
+++ src/components/view/campaign/campaign-content.js
@@ -1,11 +1,12 @@
 import React, { Suspense, lazy } from 'react';
-import { Flex, Box, Text, Heading } from '@chakra-ui/core';
+import { Flex, Box, Text, Heading, Link } from '@chakra-ui/core';
 import ReactMarkdown from 'react-markdown';
 import ChakraUIRenderer from 'chakra-ui-markdown-renderer';
 import Documents from './documents';
 import Goals from './goals';
 import Loader from '../../ui/loader';
 
+// const CampaignTarget = lazy(() => import('./campaign-target'));
 const Timeline = lazy(() => import('../../ui/timeline'));
 
 function CampaignContent({ data }) {
@@ -16,6 +17,7 @@ function CampaignContent({ data }) {
         flexShrink="0"
         maxW={{ base: '100%', lg: '65%' }}
         fontSize="15px"
+        fontWeight={500}
       >
         <ReactMarkdown
           renderers={{
@@ -23,6 +25,14 @@ function CampaignContent({ data }) {
             paragraph: props => {
               const { children } = props;
               return <Text mb={4}>{children}</Text>;
+            },
+            link: props => {
+              const { children, href } = props;
+              return (
+                <Link color="blue.500" href={href} isExternal>
+                  {children}
+                </Link>
+              );
             },
           }}
           source={data.campaign?.campaignText}
@@ -39,30 +49,44 @@ function CampaignContent({ data }) {
           </Box>
         )}
       </Box>
-      {data.campaign?.updates.length ? (
-        <Box
-          bg="gray.50"
-          borderRadius="4px"
-          p={4}
-          w="full"
-          height="full"
-          maxW={{ base: '100%' }}
-          ml={{ base: 0, lg: 8 }}
-          mt={{ base: 4, lg: 0 }}
-        >
-          <Heading size="sm" color="gray.500">
-            Kampanya Gelişmeleri
-          </Heading>
-          <Suspense fallback={<Loader />}>
-            <Timeline
-              items={data.campaign?.updates}
-              transactions={data.campaign?.transactions}
-            />
-          </Suspense>
-        </Box>
-      ) : (
-        ''
-      )}
+      <Box w="full" height="full">
+        {/* {data.campaign?.updates.length > 0 && (
+          <Box
+            bg="gray.50"
+            borderRadius="4px"
+            p={4}
+            maxW={{ base: '100%' }}
+            ml={{ base: 0, lg: 8 }}
+            mt={{ base: 4, lg: 0 }}
+            mb={8}
+          >
+            <Suspense fallback={<Loader />}>
+              <CampaignTarget />
+            </Suspense>
+          </Box>
+        )} */}
+
+        {data.campaign?.updates.length > 0 && (
+          <Box
+            bg="gray.50"
+            borderRadius="4px"
+            p={4}
+            maxW={{ base: '100%' }}
+            ml={{ base: 0, lg: 8 }}
+            mt={{ base: 4, lg: 0 }}
+          >
+            <Heading size="sm" color="gray.500">
+              Kampanya Gelişmeleri
+            </Heading>
+            <Suspense fallback={<Loader />}>
+              <Timeline
+                items={data.campaign?.updates}
+                transactions={data.campaign?.transactions}
+              />
+            </Suspense>
+          </Box>
+        )}
+      </Box>
     </Flex>
   );
 }
---
+++ src/components/view/campaign/select-bank.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { SimpleGrid, Button, Image, Box, Heading, Text } from '@chakra-ui/core';
+
+const SelectBank = ({ bankData, onSelect, selectedBank }) => {
+  return (
+    <Box>
+      <Box mb={4}>
+        <Heading as="p" mb={4} size="sm">
+          Lütfen desteğinizi göndermek istediğiniz bankayı seçiniz.
+        </Heading>
+        <Text as="p">
+          Listelenen banka hesapları, <strong>BiLira</strong> tarafından
+          sağlanan ödeme kanallarıdır. Bu banka hesaplarından birinde hesabınız
+          olmasa dahi <strong>EFT</strong> ile gönderme seçeneğiniz
+          bulunmaktadır.
+        </Text>
+      </Box>
+      <SimpleGrid columns={{ base: 2, md: 5 }} spacing={4} mb={4}>
+        {bankData.systemBankAccounts.map(bankAccount => (
+          <Button
+            key={bankAccount.id}
+            variant="ghost"
+            border="1px solid"
+            borderColor="gray.100"
+            onClick={() => onSelect(bankAccount.id)}
+            bg={selectedBank === bankAccount.id ? 'gray.100' : 'transparent'}
+          >
+            <Image
+              alt={bankAccount.name}
+              src={`${process.env.PUBLIC_URL}/images/banks/${bankAccount.id}.png`}
+            />
+          </Button>
+        ))}
+      </SimpleGrid>
+    </Box>
+  );
+};
+
+export default SelectBank;

75e25c936ed912a5c8c3db7832f16f522a1ac9fc
Mustafa Turhan <odunluzikkim@gmail.com>
2020-06-11 17:39:02 +0300

    update tests with new apollo provider

--- src/utils/test-utils.js
+++ src/utils/test-utils.js
@@ -2,27 +2,31 @@ import React from 'react';
 import { Global } from '@emotion/core';
 import { render } from '@testing-library/react';
 import { CSSReset, ThemeProvider } from '@chakra-ui/core';
+import { ApolloProvider } from '@apollo/react-hooks';
 import customTheme from '../theme';
+import client from './apollo';
 
 // eslint-disable-next-line react/prop-types
 const AllTheProviders = ({ children }) => {
   return (
-    <ThemeProvider theme={customTheme}>
-      <CSSReset />
-      <Global
-        styles={{
-          html: {
-            fontFamily:
-              'Quicksand, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
-            fontSize: '18px',
-          },
-          body: {
-            backgroundColor: '#fff',
-          },
-        }}
-      />
-      {children}
-    </ThemeProvider>
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={customTheme}>
+        <CSSReset />
+        <Global
+          styles={{
+            html: {
+              fontFamily:
+                'Quicksand, -apple-system, BlinkMacSystemFont, "Segoe UI", Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
+              fontSize: '18px',
+            },
+            body: {
+              backgroundColor: '#fff',
+            },
+          }}
+        />
+        {children}
+      </ThemeProvider>
+    </ApolloProvider>
   );
 };
 

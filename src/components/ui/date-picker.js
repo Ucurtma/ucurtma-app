20132da9e2bec71d5fcc1d67a1954db19726aa5e
Mustafa Turhan <odunluzikkim@gmail.com>
2020-06-23 16:20:34 +0300

    change date picker with 3 input version

--- src/components/ui/date-picker.js
+++ src/components/ui/date-picker.js
@@ -1,77 +1,138 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  Input as ChakraInput,
+  VisuallyHidden,
+  Input,
+  Select,
   FormErrorMessage,
   FormLabel,
   FormControl,
+  Stack,
 } from '@chakra-ui/core';
 import { useField, useFormikContext } from 'formik';
-import ReactDatePicker, { registerLocale } from 'react-datepicker';
-import tr from 'date-fns/locale/tr';
 import moment from 'moment';
+import 'moment/locale/tr';
 
-registerLocale('tr', tr);
+moment.locale('tr');
 
-const DatePickerInput = React.forwardRef(
-  (
-    { value, onClick, label, type, controlProps, field, meta, ...props },
-    ref
-  ) => {
-    return (
-      <FormControl
-        width="100%"
-        isInvalid={meta.error && meta.touched}
-        mb={4}
-        ref={ref}
-        {...controlProps}
-      >
-        {label && (
-          <FormLabel color="gray.600" htmlFor={field.name}>
-            {label}
-          </FormLabel>
-        )}
-        {/* <ChakraInput value={value} onClick={onClick} /> */}
-        <ChakraInput
-          aria-label={label || field.name}
-          aria-describedby={label || field.name}
-          type={type || 'text'}
-          errorBorderColor="red.300"
-          isInvalid={meta.touched && !!meta.error}
-          {...field}
-          {...props}
-          readOnly
-          value={value && moment(value).format('DD.MM.YYYY')}
-        />
-        {meta.touched && meta.error ? (
-          <FormErrorMessage>{meta.error}</FormErrorMessage>
-        ) : null}
-      </FormControl>
-    );
-  }
-);
-
-function DatePicker({ ...props }) {
-  const { setFieldValue } = useFormikContext();
+function DatePicker({ label, type, controlProps, ...props }) {
   const [field, meta] = useField(props);
-  const { placeholder, isReadOnly } = props;
-  return (
-    <ReactDatePicker
-      {...field}
-      {...props}
-      selected={(field.value && new Date(field.value)) || null}
-      placeholderText={placeholder}
-      onChange={val => setFieldValue(field.name, val)}
-      dateFormat="yyyy-MM-dd'T'HH:mm:ss.SSSxxx" // sending iso format to input
-      customInput={
-        <DatePickerInput
-          label="Doğum Tarihi"
-          field={field}
-          meta={meta}
-          isReadOnly={isReadOnly}
-        />
+  const [valueChange, setValueChange] = useState(false);
+  const [value, setValue] = useState(moment());
+  const [dateValues, setDateValues] = useState({
+    date: undefined,
+    month: undefined,
+    year: undefined,
+  });
+  const allChanged =
+    !!dateValues.date && !!dateValues.month && !!dateValues.year;
+  const { setFieldValue, setFieldTouched } = useFormikContext();
+
+  const years = back => {
+    const year = new Date().getFullYear();
+    return Array.from({ length: back }, (v, i) => year - back + i + 1);
+  };
+  const months = moment.months();
+  const days = Array.from(Array(32).keys()).slice(1);
+
+  const handleSelect = (e, dateType) => {
+    const inputValue = e.currentTarget.value;
+    const date = dateType === 'date' ? inputValue : dateValues.date;
+    const month =
+      dateType === 'month'
+        ? parseInt(inputValue, 10) + 1
+        : dateValues.month + 1;
+    const year = dateType === 'year' ? inputValue : dateValues.year;
+    const isValid = moment(`${date}-${month}-${year}`, 'DD-MM-YYYY').isValid();
+    setFieldTouched(field.name);
+
+    if (inputValue) {
+      setDateValues({ ...dateValues, [dateType]: parseInt(inputValue, 10) });
+      if (date && month && year) {
+        setValue(value.set(dateType, inputValue));
+        if (isValid) {
+          setFieldValue(field.name, value);
+        } else {
+          setFieldValue(field.name, 'invalidDate');
+        }
+      } else {
+        setFieldValue(field.name, 'notChanged');
       }
-      locale="tr"
-    />
+    } else {
+      setDateValues({ ...dateValues, [dateType]: '' });
+      setFieldValue(field.name, '');
+    }
+
+    setValueChange(!valueChange);
+  };
+
+  return (
+    <FormControl
+      width="100%"
+      isInvalid={meta.error && meta.touched}
+      mb={4}
+      {...controlProps}
+    >
+      {label && (
+        <FormLabel color="gray.600" htmlFor={field.name}>
+          {label}
+        </FormLabel>
+      )}
+      <VisuallyHidden
+        as={Input}
+        aria-label={label || field.name}
+        aria-describedby={label || field.name}
+        type={type || 'text'}
+        errorBorderColor="red.300"
+        isInvalid={meta.touched && !!meta.error}
+        {...field}
+        {...props}
+        readOnly
+        value={allChanged ? value && value.format('DD.MM.YYYY') : ''}
+      />
+      <Stack isInline>
+        <Select
+          placeholder="Gün"
+          value={dateValues.date}
+          onChange={e => handleSelect(e, 'date')}
+          onBlur={() => setFieldTouched(field.name, true, false)}
+        >
+          {days.map(day => (
+            <option key={day} value={day}>
+              {day}
+            </option>
+          ))}
+        </Select>
+        <Select
+          placeholder="Ay"
+          value={dateValues.month}
+          onChange={e => handleSelect(e, 'month')}
+          onBlur={() => setFieldTouched(field.name, true, false)}
+        >
+          {months.map((month, monthIndex) => (
+            <option key={month} value={monthIndex}>
+              {month}
+            </option>
+          ))}
+        </Select>
+        <Select
+          placeholder="Yıl"
+          value={dateValues.year}
+          onChange={e => handleSelect(e, 'year')}
+          onBlur={() => setFieldTouched(field.name, true, false)}
+        >
+          {years(121)
+            .reverse()
+            .map(year => (
+              <option key={year} value={year}>
+                {year}
+              </option>
+            ))}
+        </Select>
+      </Stack>
+      {meta.touched && meta.error ? (
+        <FormErrorMessage>{meta.error}</FormErrorMessage>
+      ) : null}
+    </FormControl>
   );
 }
 

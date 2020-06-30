6ab4a8e422ac12cba6cc0d0b41d3eca09736e57c
Mustafa Turhan <odunluzikkim@gmail.com>
2020-06-30 15:12:25 +0300

    remove kyc and show wallet directly

--- src/components/ui/ethereum-detail-viewer.js
+++ src/components/ui/ethereum-detail-viewer.js
@@ -5,33 +5,25 @@ import { QRCode } from 'react-qr-svg';
 function EthereumDetailViewer({ data }) {
   return (
     <Flex align="center">
-      <Link
-        mr={4}
-        isExternal
-        href={`https://etherscan.io/address/${data.collectEthDonation.address}`}
-      >
+      <Link mr={4} isExternal href={`https://etherscan.io/address/${data}`}>
         <Box flexShrink="0">
           <QRCode
             bgColor="#FFFFFF"
             fgColor="#000000"
             level="Q"
             style={{ width: 200, padding: '1rem' }}
-            value={`https://etherscan.io/address/${data.collectEthDonation.address}`}
+            value={`https://etherscan.io/address/${data}`}
           />
         </Box>
         {/* <Image
           flexShrink="0"
-          src={`https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=${data.collectEthDonation.address}`}
+          src={`https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=${data}`}
         /> */}
       </Link>
       <Box>
         <Box mb={4}>
           <Heading size="sm">Ethereum Adresi:</Heading>
-          <Text wordBreak="break-all">{data.collectEthDonation.address}</Text>
-        </Box>
-        <Box>
-          <Heading size="sm">Göndereceğiniz Destek Miktarı:</Heading>
-          <Text wordBreak="break-all">{data.collectEthDonation.amount}</Text>
+          <Text wordBreak="break-all">{data}</Text>
         </Box>
       </Box>
     </Flex>

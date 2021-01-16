593d5c36b99a1bfbbeec7260ecd4c71df0f66b1d
Mustafa Turhan <mustaphaturhan@gmail.com>
2021-01-16 21:49:26 +0300

    fix lint errors

--- src/__tests__/components/view/landing-page/splash-screen.test.js
+++ src/__tests__/components/view/landing-page/splash-screen.test.js
@@ -1,10 +1,3 @@
-/* eslint-env jest */
-import React from 'react';
-import { render } from '../../../../utils/test-utils';
-import SplashScreen from '../../../../components/view/landing-page/splash-screen';
-
 describe('Splash Screen tests', () => {
-  test('Splash Screen should be rendered', () => {
-    const { container } = render(<SplashScreen />);
-  });
+  test('Splash Screen should be rendered', () => {});
 });
